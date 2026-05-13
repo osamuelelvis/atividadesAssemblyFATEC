b1ac0990386b647f9ac1882235d04c4b1f9f8db3
osamuelelvis <elvislimasamuel27@gmail.com>
2026-05-13 10:18:27 -0300

    corrected exercise 3 from list 3

--- Lista3Exercicio3.asm
+++ Lista3Exercicio3.asm
@@ -2,8 +2,8 @@
 	valor1: .asciiz "Digite o primeiro valor inteiro: "
 	valor2: .asciiz "Digite o segundo valor inteiro: "
 	maior: .asciiz "Maior: "
-	menor: .asciiz "Menor: "
-	diferenca: .asciiz "A diferença do maior para o menor número é igual a: "
+	menor: .asciiz "\nMenor: "
+	diferenca: .asciiz "\nA diferença do maior para o menor número é igual a: "
 .text
 main:
 	li $v0, 4
@@ -22,4 +22,61 @@ main:
 	syscall
 	move $t1, $v0
 	
+	bgt $t0, $t1, t0_maior
 	
+t1_maior:
+	li $v0, 4
+	la $a0, maior
+	syscall
+	
+	move $a0, $t1
+	li $v0, 1
+	syscall
+	
+	li $v0, 4
+	la $a0, menor
+	syscall
+	
+	move $a0, $t0
+	li $v0, 1
+	syscall
+	
+	sub $t2, $t1, $t0
+	
+	li $v0, 4
+	la $a0, diferenca
+	syscall
+	
+	move $a0, $t2
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall
+
+t0_maior:
+	li $v0, 4
+	la $a0, maior
+	syscall
+	
+	move $a0, $t0
+	li $v0, 1
+	syscall
+	
+	li $v0, 4
+	la $a0, menor
+	syscall
+	
+	move $a0, $t1
+	li $v0, 1
+	syscall
+	
+	sub $t2, $t0, $t1
+	
+	li $v0, 4
+	la $a0, diferenca
+	syscall
+	
+	move $a0, $t2
+	li $v0, 1
+	syscall
